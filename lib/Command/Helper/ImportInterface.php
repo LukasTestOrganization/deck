ef7327c2f7f16c83e5eba760a47dc2e2e8928f35
Vitor Mattos <vitor@php.rio>
2021-07-12 11:04:23 -0300

    psalm
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Command/Helper/ImportInterface.php
+++ lib/Command/Helper/ImportInterface.php
@@ -23,6 +23,7 @@
 
 namespace OCA\Deck\Command\Helper;
 
+use OCA\Deck\Command\BoardImport;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
